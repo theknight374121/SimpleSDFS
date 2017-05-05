cd3d678400ce601c55864fbb523b30007b5e14ba
Amey Patil <theknight374121@gmail.com>
2017-05-05 15:38:01 -0400

    Added threaded support for multi-client environment

--- client.py
+++ client.py
@@ -9,21 +9,47 @@
 
 import socket, ssl
 import sys
+import getpass
 
-def setupConn():
+def printListandChoice():
+	'''
+	Input: None
+	Return: Choice (Integer)
+	Desc: Prints the Menu for Clients and asks them to choose one options, then return the choose one
+	'''
+	print "To be Continued"
+
+def setupConn(hostname):
 	#Initialize context
 	ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
 	ctx.load_verify_locations('./demoCA/cacert.pem')
 		
-	#ssetup client
-	#ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-	#context.verify_mode = ssl.CERT_REQUIRED
-	#context.check_hostname = True
-	#context.load_verify_locations("/etc/ssl/certs/ca-bundle.crt")
-	ssl_conn = ctx.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM),server_hostname=sys.argv[1])
-	ssl_conn.connect((sys.argv[1], 33333))
-	ssl_conn.send('Hello')
-	ssl_conn.close()
+	#Setup Client socket
+	ssl_conn = ctx.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM),server_hostname=hostname)
+	
+	#Connect to Server
+	ssl_conn.connect((hostname, 33333))
+	
+	#Perform Login or Account Creation
+	login_failed = True
+	while login_failed:
+		choice = int(raw_input("1-Login\n2-Create Account\nEnter Choice: "))
+		if choice == 1:
+			#Let Server know that login is attempted
+			ssl_conn.send("1")
+			return 
+		 	# TODO Convert this input to join 
+			username = raw_input("Username:")
+			password = getpass.getpass()
+			payload=username+"|"+password
+			ssl_conn.send(payload)
+		elif choice == 2:
+			#Let server know that account creation is attempted
+			ssl_conn.send("2")
+			#Create an account
+		else:
+			print "Invalid Option! Try Again \n"
+	
 	
 def initiateConn():
     cli_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,7 +58,12 @@ def initiateConn():
     cli_socket.close()
 
 def main():
-    setupConn()
+	if len(sys.argv) < 2:
+		print "You need to provide hostname of server. Needed for Server Cert Verification"
+		return -1
+	else:
+		hostname = sys.argv[1]
+		setupConn(hostname)
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -7,35 +7,72 @@
 #
 ############################################################################
 
-import ssl
-import socket
+import ssl, socket
+from threading import Thread
+
+class HandleClients(Thread):
+	'''
+	Thread Class to Handle Client Connections
+	'''
+	def __init__(self, sock, cli_details):
+		Thread.__init__(self)
+		self.sock = sock
+		self.cli_addr = cli_details[0]
+		self.cli_port = cli_details[1]
+		print "Started a Thread for Client with addr={} and port={}".format(self.cli_addr, self.cli_port)
+
+	def run(self):
+		#Check what is being attempted
+		choice=int(self.sock.recv(1))
+		if choice == 1:
+			#Handle login
+			print "Reached Login"
+		elif choice == 2:
+			#Handle account creation
+			print "TP"
+		else:
+			#Should never reach here. Redundant.
+			print "Something wrong happened inside Login handling of thread that was handling Client with addr:{} and port:{}".format(self.cli_addr, self.cli_port)
+
+		self.sock.close()
 
 def setupSSL():
+	#Variables
+	threads = list()
+
 	#Initialize context
 	ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 	ctx.load_cert_chain(certfile="server_signed_cert.pem", keyfile="./key/server_priv.pem")
-	
-	#SetupServer
+
+	#Setup SSL Server Socket
 	server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_sock.bind(('',33333))
-	server_sock.listen(1)
-	newsock, addr = server_sock.accept()
-	ssl_sock = ctx.wrap_socket(newsock, server_side=True)
-	data=ssl_sock.recv(4096)
-	print data
-	ssl_sock.close()
-	
-def initiateServer():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 33333))
-    server_socket.listen(1)
-    (cli_socket, address) = server_socket.accept()
-    print address
-    rec_data=cli_socket.recv(4096)
-    print rec_data
-    server_socket.close()
-    cli_socket.close()
 
+	#Queue up max of 2 connections
+	server_sock.listen(2)
+
+	#This loop will never end hence add try catch
+	try:
+		#Handle those connections using a custom thread class
+		while True:
+			cli_sock, addr = server_sock.accept()
+			ssl_sock = ctx.wrap_socket(cli_sock, server_side=True)
+	
+			#Create a thread to handle this connection
+			newthread= HandleClients(ssl_sock, addr)
+			newthread.start()
+	
+			#Append this thread in a list to keep track
+			threads.append(newthread)
+	finally:
+		#Close all sockets
+		ssl_sock.close()
+		server_sock.close()
+
+		#Call for join on all threads before exiting
+		for t in threads:
+			t.join()
+	
 def main():
     setupSSL()
 
