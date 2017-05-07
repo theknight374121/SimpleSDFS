b5a0c74b41b5e9b80c9b016a7d5bbae252f99307
Amey Patil <theknight374121@gmail.com>
2017-05-07 16:54:38 -0400

    Added Client Validation using Password Hashing. Added Set permission, Delegate Permission, Read-Write Files and Logout.

--- client.py
+++ client.py
@@ -17,7 +17,145 @@ def printListandChoice():
 	Return: Choice (Integer)
 	Desc: Prints the Menu for Clients and asks them to choose one options, then return the choose one
 	'''
-	print "To be Continued"
+	while True:
+		print "1-Create File"
+		print "2-Read File"
+		print "3-Write File"
+		print "4-SetPermission"
+		print "5-Delegate Permission"
+		print "6-Logout"
+		choice=int(raw_input("Enter your Choice:"))
+		if choice < 7 and choice > 0:
+			return choice
+		else:
+			print "Choice should be withn 1-6! Try Again!"
+			continue
+
+def handleMenu(ssl_conn):
+	while True:
+		choice=printListandChoice()
+		if choice == 1:
+			ssl_conn.send("1")
+			handleCreateFile(ssl_conn)
+		elif choice == 2:
+			ssl_conn.send("2")
+			handleReadFile(ssl_conn)
+		elif choice == 3:
+			ssl_conn.send("3")
+			handleWriteFile(ssl_conn)
+		elif choice == 4:
+			ssl_conn.send("4")
+			handleSetPermission(ssl_conn)
+		elif choice == 5:
+			ssl_conn.send("5")
+			handleDelegatePermission(ssl_conn)
+		elif choice ==6:
+			ssl_conn.send("6")
+			break
+	return 
+
+def handleCreateFile(ssl_conn):
+	filename = raw_input("Enter filename:")
+	
+	#Send server filename
+	sendPayload(ssl_conn, filename)
+
+	#Receive server respsonse
+	response=int(ssl_conn.recv(1))
+
+	if response==1:
+		print "Done creating File!"
+	else:
+		print "Error while creating File"
+	return
+
+def handleReadFile(ssl_conn):
+	#Ask user filename
+	filename = raw_input("Enter the filename:")
+	#Check if this user has access to write to this file
+	if hasFileAccess(ssl_conn, filename):
+		#Receive the data
+		data=receivePayload(ssl_conn)
+		#Print the response
+		print data
+	else:
+		print "No Access to read the file!"
+	return
+	
+def handleWriteFile(ssl_conn):
+	#Ask user filename
+	filename = raw_input("Enter the filename:")
+	#Check if this user has access to write to this file
+	if hasFileAccess(ssl_conn, filename):
+		#Ask for data
+		data=raw_input("Enter data:")
+		#Send the data
+		sendPayload(ssl_conn, data)
+		#Get the response
+		response = int(ssl_conn.recv(1))
+		if response == 1:
+			print "Successfully added data"
+		else:
+			print "Error in adding data"
+	else:
+		print "No Access to write to this file!"
+	return
+
+def handleSetPermission(ssl_conn):
+	#Ask filename you want to set permission for
+	filename=raw_input("Enter filename:")
+
+	#Check if this user has access to modify permissions of this file
+	if hasFileAccess(ssl_conn, filename):
+		#IF yes, ask the username and the permissions
+		user=raw_input("Enter the user's name you want to give access:")
+		permission=raw_input("Permission(R/W/RW):")
+
+		#Creating the payload
+		payload=list()
+		payload.append(user)
+		payload.append(permission)
+		payload_str="|".join(payload)
+
+		#Send payload
+		sendPayload(ssl_conn, payload_str)
+
+		#Receive server response
+		response=int(ssl_conn.recv(1))
+		if response==1:
+			print "Successfully added permissions!!"
+			return
+		else:
+			print "Error in adding permissions"
+			return
+	else:
+		print "Access Denied! Only owner of the file can add permissions"
+		return
+
+def hasFileAccess(ssl_conn, filename):
+	#Send server file name to check if user has access
+	sendPayload(ssl_conn,filename)
+
+	#Check server response
+	response=int(ssl_conn.recv(1))
+	
+	if response == 1:
+		return True
+	else:
+		return False
+
+def handleDelegatePermission():
+	pass
+
+def sendPayload(ssl_conn, data):
+	len_str = str(len(data))
+	pad = '0'*(5-len(len_str))
+	return ssl_conn.send(pad+len_str+data)
+
+def receivePayload(ssl_conn):
+	length=int(ssl_conn.recv(5))
+	payload = ssl_conn.recv(length)
+	return payload
 
 def setupConn(hostname):
 	#Initialize context
@@ -29,20 +167,36 @@ def setupConn(hostname):
 	
 	#Connect to Server
 	ssl_conn.connect((hostname, 33333))
-	
+
+	return ssl_conn
+
+def handleLogin(ssl_conn):
 	#Perform Login or Account Creation
 	login_failed = True
+	payload=list()
 	while login_failed:
 		choice = int(raw_input("1-Login\n2-Create Account\nEnter Choice: "))
 		if choice == 1:
 			#Let Server know that login is attempted
 			ssl_conn.send("1")
-			return 
-		 	# TODO Convert this input to join 
-			username = raw_input("Username:")
-			password = getpass.getpass()
-			payload=username+"|"+password
-			ssl_conn.send(payload)
+
+		 	#User input for login 
+			payload.append(raw_input("Username: "))
+			payload.append(getpass.getpass())
+			payload_str="|".join(payload)
+			
+			#Send login payload
+			sendPayload(ssl_conn, payload_str)
+
+			#Receive result
+			result=int(ssl_conn.recv(1))
+			if result == 0:
+				#Login failed
+				continue
+			else:
+				#Login succeeded
+				return True
+
 		elif choice == 2:
 			#Let server know that account creation is attempted
 			ssl_conn.send("2")
@@ -63,7 +217,12 @@ def main():
 		return -1
 	else:
 		hostname = sys.argv[1]
-		setupConn(hostname)
+		#Setup SSL
+		ssl_conn=setupConn(hostname)
+		#Handle Login
+		if handleLogin(ssl_conn):
+			#Run Menu Program
+			handleMenu(ssl_conn)
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -9,34 +9,278 @@
 
 import ssl, socket
 from threading import Thread
+import MySQLdb
+import getpass
+import hashlib
 
 class HandleClients(Thread):
 	'''
 	Thread Class to Handle Client Connections
 	'''
-	def __init__(self, sock, cli_details):
+	def __init__(self, sock, cli_details, dbconn):
 		Thread.__init__(self)
 		self.sock = sock
 		self.cli_addr = cli_details[0]
 		self.cli_port = cli_details[1]
+		self.db = dbconn
+		self.cli_username=""
 		print "Started a Thread for Client with addr={} and port={}".format(self.cli_addr, self.cli_port)
 
 	def run(self):
-		#Check what is being attempted
-		choice=int(self.sock.recv(1))
-		if choice == 1:
-			#Handle login
-			print "Reached Login"
-		elif choice == 2:
-			#Handle account creation
-			print "TP"
+		#Handle initial login
+		while True:
+			#Check what is being attempted
+			choice=int(self.sock.recv(1))
+			if choice == 1:
+				#Handle login
+				if self.login():
+					print "Login Successful!"
+					# 1 to indicate success
+					self.sock.send("1")
+					break
+				else:
+					# 0  to indicate failure
+					self.sock.send("0")
+					continue
+			elif choice == 2:
+				#Handle account creation
+				print "TP"
+			else:
+				#Should never reach here. Redundant.
+				print "Something wrong happened inside Login handling of thread that was handling Client with addr:{} and port:{}".format(self.cli_addr, self.cli_port)
+
+		#Listen for other instructions now
+		while True:
+			#Check what is being attempted:
+			choice=int(self.sock.recv(1))
+			if choice == 1:
+				self.handleCreateFile()
+			elif choice == 2:
+				self.handleReadFile()
+			elif choice == 3:
+				self.handleWriteFile()
+			elif choice == 4:
+				self.handleSetPermission()
+			elif choice == 5:
+				self.handleDelegatePermission()
+			elif choice == 6:
+				self.logout()
+				break
+		#Exit thread			
+		return
+
+	def handleCreateFile(self):
+		#Receive File name to create
+		filename = self.receivePayload()
+
+		#Create the file and add entry of it to the database
+		with open(filename, "w") as file_obj:
+			file_obj.close()
+
+		#Add this filename to database
+		query = ''' INSERT INTO files (filename, RDONLY, WRONLY, owner)  values (%s, %s, %s, %s)'''
+		r_perm = self.cli_username
+		w_perm = self.cli_username
+		owner = self.cli_username
+		values = (filename, r_perm, w_perm, owner)
+		cursor = self.db.cursor()
+		
+		try:
+			result=cursor.execute(query, values)
+			self.db.commit()
+		except MySQLdb.Error, e:
+			print "Query error {}".format(str(e))
+
+		#Add code to check if the query did successfully execute
+		print result
+
+		#Return success message
+		self.sock.send("1")
+
+		return
+
+	def handleReadFile(self):
+		#Receive Filename
+		filename = self.receivePayload()
+		#Check if has access
+		if self.hasFileAccess(filename, 'R'):
+			#IF yes pull data
+			with open(filename, "r") as file_obj:
+				data_list=file_obj.readlines()
+				data_str="".join(data_list)
+				self.sendPayload(data_str)
+				file_obj.close()
+		return
+
+	def handleWriteFile(self):
+		#Receive Filename
+		filename = self.receivePayload()
+		#Check if has access
+		if self.hasFileAccess(filename, 'W'):
+			#IF yes , receive data and add it to the file
+			data = self.receivePayload()
+			with open(filename, "w") as file_obj:
+				file_obj.write(data)
+				file_obj.close()
+			#Return success message
+			self.sock.send("1")
+
+			return
+		
+	def hasFileAccess(self, filename, perm):
+		#Check if the client is owner of the file
+		query=''' SELECT RDONLY,WRONLY FROM files WHERE filename = %s'''
+		values = (filename,)
+		cursor = self.db.cursor()
+
+		cursor.execute(query, values)
+		r_perm, w_perm = cursor.fetchone()
+		
+		if perm == 'R':
+			#Check if user has read access
+			if self.cli_username in r_perm:
+				self.sock.send("1")
+				return True
+			else:
+				self.sock.send("0")
+				return False
+		elif perm == 'W':
+			#Check if user has write access
+			if self.cli_username in w_perm:
+				self.sock.send("1")
+				return True
+			else:
+				self.sock.send("0")
+				return False
+
+			
+	def isOwner(self, filename):
+		#Check if the client is owner of the file
+		query=''' SELECT owner FROM files WHERE filename = %s'''
+		values = (filename,)
+		cursor = self.db.cursor()
+
+		cursor.execute(query, values)
+		owner = cursor.fetchone()
+		
+		if owner[0] == self.cli_username:
+			#Send the Client that he has access
+			self.sock.send("1")
+			return True
 		else:
-			#Should never reach here. Redundant.
-			print "Something wrong happened inside Login handling of thread that was handling Client with addr:{} and port:{}".format(self.cli_addr, self.cli_port)
+			#Send Client that access denied
+			self.sock.send("0")
+			return False		
+	
+	def handleSetPermission(self):
+		#Receive Filename from user
+		filename=self.receivePayload()
 
+		if self.isOwner(filename):
+			#Fetch current permissions
+			query = '''SELECT RDONLY, WRONLY FROM files WHERE filename = %s'''
+			values = (filename,)
+			cursor.execute(query, values)
+			r_perm_e, w_perm_e = cursor.fetchone()
+
+			print r_perm_e
+			#if the owner is the user then set permissions
+			data=self.receivePayload()
+			data_list=data.split("|")
+			#Set user
+			user=data_list[0]
+			#Set permission
+			if data_list[1] == "R":
+				r_perm = r_perm_e+":"+user
+				values = (r_perm,filename)
+			elif data_list[1] == "W":
+				w_perm = w_perm_e+":"+user
+				values = (w_perm,filename)
+			elif data_list[1] == "RW":
+				r_perm = r_perm_e+":"+user
+				w_perm = w_perm_e+":"+user
+				values = (r_perm, w_perm, filename)
+
+			#Only supports read write
+			query = ''' UPDATE files SET RDONLY=%s, WRONLY=%s WHERE filename=%s '''
+			cursor.execute(query, values)
+			self.db.commit()
+			
+			#Send success message
+			self.sock.send("1")
+
+		return
+
+	def handleDelegatePermission(self):
+		pass
+
+	def logout(self):
+		#Cleanua
 		self.sock.close()
+		self.db.close()
 
-def setupSSL():
+		#Logout message
+		print "Logging Out!"
+		return 
+	
+	def sendPayload(self, data):
+		len_str = str(len(data))
+		pad = '0'*(5-len(len_str))
+		return self.sock.send(pad+len_str+data)
+
+
+	def receivePayload(self):
+		length=int(self.sock.recv(5))
+		payload = self.sock.recv(length)
+		return payload
+
+	def login(self):
+		#Receive Data
+		payload=self.receivePayload()
+		
+		#Format the data
+		payload_list = payload.split('|')
+		username=payload_list[0]
+		print "Username is {}".format(username)
+		passwd = payload_list[1]
+		
+		#Prepare DB objects
+		cursor = self.db.cursor()
+		
+		#Fetch Salt
+		query = ''' select salt, password from users where name=%s '''
+		values = (username,)
+		cursor.execute(query, values)
+		dbsalt, dbpwd = cursor.fetchone()
+		#print "SAlt: {} and pwd:{}".format(dbsalt, dbpwd)
+		hashpwd = hashlib.sha256(dbsalt+passwd).hexdigest()
+		
+		if hashpwd == dbpwd:
+			self.cli_username=username
+			return True
+		else:
+			return False
+
+	def accountCreation(self):
+		pass		
+
+def setupDBConn():
+	'''
+	Set up connection to the Database
+	'''
+	#Fetch user password
+	password=getpass.getpass("Enter Database Password:")
+
+	#Initialize DB Connection
+	try:
+		db=MySQLdb.connect(host="127.0.0.1",user="CIS600",passwd=password,db="SimpleSDFS")
+	except MySQLdb.Error, e:
+		print "Could not connect to Database. Error message: %s".format(str(e))
+
+	print "Successfully Connected to Database!!"
+	return db
+
+def setupSSL(dbconn):
 	#Variables
 	threads = list()
 
@@ -59,7 +303,7 @@ def setupSSL():
 			ssl_sock = ctx.wrap_socket(cli_sock, server_side=True)
 	
 			#Create a thread to handle this connection
-			newthread= HandleClients(ssl_sock, addr)
+			newthread= HandleClients(ssl_sock, addr, dbconn)
 			newthread.start()
 	
 			#Append this thread in a list to keep track
@@ -73,8 +317,13 @@ def setupSSL():
 		for t in threads:
 			t.join()
 	
+def cleanup():
+	pass
+
 def main():
-    setupSSL()
+	#Connect to DB
+	dbconn=setupDBConn()
+	setupSSL(dbconn)
 
 if __name__ == '__main__':
     main()
